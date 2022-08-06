9827534a99f9bfe4466ad19eec447f4f3f184d27
rick <ysdvictor@outlook.com>
2022-08-06 23:38:15 +0800

    手写复习

--- JavaScript手写/exercise.js
+++ JavaScript手写/exercise.js
@@ -1,17 +1,60 @@
-Function.prototype.bind = function(context) {
-    context = context || window;
-    let args = arguments.slice(1);
-    let symbolFn = Symbol('key');
-    context[symbolFn] = this;
-    let _this = this;
-    function results (...params) {
-        if (this instanceof _this) {
-            this[symbolFn] = _this;
-            return this[symbolFn](...[...args, ...params]);
-        }
-        return context[symbolFn](...[...args, ...params]);
-    }
+// Function.prototype.bind = function(context) {
+//     context = context || window;
+//     let args = arguments.slice(1);
+//     let symbolFn = Symbol('key');
+//     context[symbolFn] = this;
+//     let _this = this;
+//     function results (...params) {
+//         if (this instanceof _this) {
+//             this[symbolFn] = _this;
+//             return this[symbolFn](...[...args, ...params]);
+//         }
+//         return context[symbolFn](...[...args, ...params]);
+//     }
 
-    results.prototype = Object.create(this.prototype);
-    return results;
+//     results.prototype = Object.create(this.prototype);
+//     return results;
+// }
+
+// 模拟 call 
+// 主要思想是通过在要绑定的对象上添加方法属性来实现
+Function.prototype.call = function(context) {
+    context = context || window;
+    let args = [...arguments].slice(1);
+    let symbolKey = Symbol('key');
+    context[symbolKey] = this;
+    let result = context[symbolKey](...args);
+    delete context[symbolKey];
+    return result;
+}
+
+// 模拟 apply
+Function.prototype.apply = function(context) {
+    context = context || window;
+    let args = [...arguments].slice(1);
+    let symbolKey = Symbol('key');
+    context[symbolKey] = this;
+    let result = context[symbolKey](args);
+    delete context[symbolKey];
+    return result;
+}
+
+// 模拟 bind
+// bind 是返回一个函数，调用的时候，也可以通过 new 的方式进行
+Function.prototype.call = function(context) {
+    context = context || window;
+    let args = [...arguments].slice(1);
+    let symbolKey = Symbol('key');
+    context[symbolKey] = this;
+    let _this = this;
+    let result = function(...params) {
+        const allParams = [...args, ...params];
+        if (this instanceof _this) {
+            return this[symbolKey](...allParams);
+        } else {
+            return context[symbolKey](...allParams);
+        }
+    }
+    result.prototype = Object.create(this.prototype);
+    return result;
 }
