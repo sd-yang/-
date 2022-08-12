1244c01f3bc0e59ec34b422ceb26f22c20a66d13
rick <ysdvictor@outlook.com>
2022-08-12 23:21:26 +0800

    feat: normal

--- JavaScript手写/数组去重.js
+++ JavaScript手写/数组去重.js
@@ -8,7 +8,23 @@ const unionArray = (array) => {
     return arr;
 }
 
+// 采用双指针 需要先进行有序化
+const unionArray2 = (array) => {
+    let slow = 0;
+    let fast = 0;
+    array.sort();
+    while(fast < array.length) {
+        if (array[slow] !== array[fast]) {
+            slow++;
+            array[slow] = array[fast];
+        }
+        fast++;
+    }
+
+    return array.slice(0, slow + 1);
+}
+
 const list = [1, 1, 1, 3, 3, 4, 5, 6, 3, 32, 2];
 
-console.log(unionArray(list))
+console.log(unionArray2(list))
 
